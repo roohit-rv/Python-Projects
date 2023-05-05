de9084ffa989025c23265d246741936cfe385f44
Rohit Verma <94777918+roohit-rv@users.noreply.github.com>
2023-05-05 00:50:15 -0800

    Delete choose_random_bill_payer.py

--- choose_random_bill_payer.py
+++
@@ -1,9 +0,0 @@
-import random
-name=input("enter everybody's name: ")
-names=name.split(",")
-l=len(names)
-length=l-1
-number=random.randint(0,length)
-payer=names[number]
-print(payer, " will pay today's bill boiiii")
-
